2e01fb6e9c66458228a3444a5a42ba631d5df898
Wonseok Chae <wochae@microsoft.com>
2018-11-02 00:47:31 -0700

    add ropsten network

--- truffle-config.js
+++ truffle-config.js
@@ -12,12 +12,22 @@
  *   },
  */
 
+const HDWalletProvider = require("truffle-hdwallet-provider");
+const MNEMONIC = process.env.MNEMONIC;
+const INFURA_ENDPOINT = process.env.INFURA_ENDPOINT;
+
 module.exports = {
   networks: {
     development: {
       host: "localhost",
       port: 8545,
       network_id: "*" // Match any newtork id
+    },
+    ropsten: {
+      provider: function() {
+        return new HDWalletProvider(MNEMONIC, INFURA_ENDPOINT);
+      },
+      network_id: 3
     }
   }
 };
